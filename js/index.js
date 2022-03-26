990cd9a72a5ce27f231ab16e224f807fb22b93ee
Bsalvo <bruno95bh@gmail.com>
2022-03-26 07:46:18 -0300

    continuação

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import Mandalas from './mandalas.js'
 import TypeEffect from './typeEffect.js'
-
+const timeout = ms => new Promise(resolve => setTimeout(resolve,ms))
 const mandala = new Mandalas('mandalas')
 setImageProfile()
 Skills.Preset.forEach(Skills.setSkill)
@@ -15,5 +15,17 @@ const presentationList = [
     'Bruno Salvo',
 ]
 const presentation = new TypeEffect('presentation',presentationList)
+
+const btn_add_petala = document.getElementById("mandala_add_petela")
+const setPetalas = async () => {
+    for (let i = 0; i < 30; i++) {
+        await timeout(300)
+        btn_add_petala.click()
+    }
+}
+setPetalas()
+
+
+
 
 
--- js/mandalas.js
+++ js/mandalas.js
@@ -1,36 +1,70 @@
 export default class Mandalas{
     #mandalaElem
+    add_Petela
+    #petalas
+    #view
 
     constructor(element){
         this.mandala = element
+        this.#petalas = []
+        this.add_Petela = this.addPetala.bind(this)
+        this.#view = document.createElement("div")
+        this.#view.classList.add("view")
         this.controller()
+    }
+
+    addPetala(){
+        let petala = new Petala()
+        let qntPetalas = this.#petalas.length
+        this.#petalas.push(petala)
+        this.#view.append(petala.add(qntPetalas))
     }
 
     controller(){
         let add_button = document.createElement("i")
         add_button.setAttribute("title","Adicionar Pétala")
+        add_button.setAttribute("id","mandala_add_petela")
         add_button.classList.add("fas","fa-plus")
+        add_button.addEventListener("click",this.add_Petela)
 
         let remove_button = document.createElement("i")
         remove_button.setAttribute("title","Remover Pétala")
         remove_button.classList.add("fas","fa-minus")
-
+        
         let controller = document.createElement("div")
         controller.classList.add("controller")
 
         controller.append(add_button,remove_button)
         this.#mandalaElem.append(controller)
-    }
 
+        this.#mandalaElem.append(this.#view)
+    }
     // Utilizada para passagem de parâmetros através de objetos 
     update(options){
         Object.entries(options).forEach(([key,value]) => {
             this[key] = value
         }) 
     }
-
+    
     set mandala(value){
         this.#mandalaElem = document.getElementById(value)
         this.#mandalaElem.classList.add("mandalas")
+    }
+}
+
+class Petala{
+    #petalaElem
+    #wasInserted = false
+    constructor(){
+        this.#petalaElem = document.createElement("div")
+        this.#petalaElem.classList.add("petala")
+    }
+
+    add(qnt){
+        if(this.#wasInserted) return
+        this.#wasInserted = true
+        this.#petalaElem.style.setProperty('--rotate',`${qnt * 10}deg`)
+        this.#petalaElem.style.setProperty('--scale',`${Math.random()}`)
+        return this.#petalaElem
     }
 }
