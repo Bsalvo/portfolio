fb780ef63ca2b2f3dc3d48ea5da090fb309da194
Bsalvo <bruno95bh@gmail.com>
2022-04-02 00:03:29 -0300

    criação da classe Form

--- js/form.js
+++ js/form.js
@@ -1,60 +1,182 @@
-export default class Form{
+const DEFAULT_FORM_STYLE = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px'
+}
+export default class Form {
     #name
     #action
     #class
-    #css
+    #style
+    #inputs = []
     #formEl
     #rendered = false
     #parent
 
-    constructor(options){
+    constructor(options) {
         this.#name = options.name
-        this.#action = options.action 
+        this.#action = options.action
         this.#class = options.class ?? undefined
-        this.#css = options.css ?? undefined
+        this.#style = options.style ?? undefined
 
         this.#formEl = document.createElement('form')
-        this.#formEl.setAttribute('action',this.#action)
-        this.#formEl.setAttribute('name',this.#name)
-        if(options.el){
-            let el = document.querySelector(options.el)
-            if(el){
-                this.#parent = el
-                el.append(this.#formEl)
-                this.#rendered = true
-            } else {
-                console.error('Elemento que o form deveria ser renderizado não existe')
+        this.#formEl.setAttribute('action', this.#action)
+        this.#formEl.setAttribute('name', this.#name)
+        css(this.#formEl, { ...DEFAULT_FORM_STYLE, ...this.#style?.form })
+        if (options.parent) {
+            this.parent = options.parent
+        }
+        if (options.inputs) {
+            this.setInputs(options.inputs)
+        }
+    }
+
+    set parent(value) {
+        let el = document.querySelector(value)
+        if (el) {
+            el.append(this.#formEl)
+            this.#parent = el
+            this.#rendered = true
+        } else {
+            console.error('Elemento que o form deveria ser renderizado não existe')
+        }
+    }
+
+    setInputs(value) {
+        value.map(input => {
+            input.parent = this.#formEl
+            let styling = {group:undefined,label:undefined,input:undefined}
+            styling.group = {...this.#style?.group,...input.style?.group}
+            styling.label = {...this.#style?.label,...input.style?.label}
+            styling.input = {...this.#style?.input,...input.style?.input}
+            input.style = styling
+            this.#inputs.push(new Input(input))
+        })
+    }
+
+    validate(){
+        this.#inputs.map(input => {
+            input.validate
+        })
+    }
+
+}
+
+const DEFAULT_INPUT_GROUP_STYLE = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '5px'
+}
+const DEFAULT_INPUT_LABEL = {
+    fontSize: '16px'
+}
+const DEFAULT_INPUT = {
+    borderRadius: '.2em',
+    padding: '.3em'
+}
+class Input {
+    #value
+    #name
+    #class
+    #style
+    #validation
+    #parent
+    #inputEl
+    #labelEl
+    #labelText
+    #type
+    #rendered = false
+    #placeholder
+
+    constructor(options) {
+        this.#name = options.name
+        this.#parent = options.parent
+        this.#style = options.style
+        this.agroup(options.label, options.type)
+        this.placeholder = options?.placeholder
+        this.value = options.value
+        this.validation = options.validation
+    }
+
+    set label(value) {
+        this.#labelText = value
+        let label = document.createElement('label')
+        label.setAttribute('for', this.#name)
+        label.textContent = this.#labelText
+        css(label,{...DEFAULT_INPUT_LABEL,...this.#style?.label})
+        this.#labelEl = label
+        
+    }
+
+    set input(value) {
+        this.#type = value
+        let el = document.createElement('input')
+        el.setAttribute('type', this.#type)
+        el.setAttribute('name', this.#name)
+        css(el,{...DEFAULT_INPUT,...this.#style?.input})
+        this.#inputEl = el
+    }
+
+    set placeholder(value){
+        if(!value) return
+        this.#placeholder = value
+        this.#inputEl.placeholder = this.#placeholder
+    }
+
+    set value(value){
+        if(!value) return
+        this.#value = value
+        this.#inputEl.value = this.#value
+    }
+
+    set validation(value){
+        if(!value) return
+        this.#validation = new Validator(value)
+    }
+
+    agroup(label, type) {
+        if (label) {
+            this.label = label
+        }
+        this.input = type ?? 'text'
+        let div = document.createElement('div')
+        css(div, { ...DEFAULT_INPUT_GROUP_STYLE, ...this.#style?.group })
+        div.append(this.#labelEl, this.#inputEl)
+        this.#parent.append(div)
+        this.#rendered = true
+    }
+
+    get validate(){
+        this.#validation.validate(this.#inputEl)
+    }
+
+}
+
+class Validator{
+    #type
+    #message
+
+    constructor(options){
+        this.#type = options.type
+        this.#message = options.message
+    }
+
+    validate(input){
+        if(this.#type == 'required'){
+            if(input.value.length == 0){
+                input.style.borderSize = '1px'
+                input.style.borderColor = '#B00020'
             }
         }
     }
-}
-
-class Input{
-    #name
-    #class
-    #css
-    #rendered = false
-    #parent
-    #type
-    #validation
-    #label
-    #labelEl
-
-    constructor(options){
-        this.#name = options.name
-        this.#parent = options.parent
-        this.#type = options.type = 'text'
-        this.#label = options.label ?? undefined
-        
-        if(this.#label){
-            let label = document.createElement('label')
-            label.setAttribute('for',this.#name)
-            label.textContent = this.#label
-            this.#labelEl = label
-        }
-
-
-    }
-
 
 }
+
+const css = (el, style) => {
+    let keys = Object.keys(style)
+    keys.map((value) => {
+        el.style[value] = style[value]
+    })
+}
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import Mandalas from './mandalas.js'
 import TypeEffect from './typeEffect.js'
 import Form from './form.js'
-const timeout = ms => new Promise(resolve => setTimeout(resolve,ms))
+const timeout = ms => new Promise(resolve => setTimeout(resolve, ms))
 //const mandala = new Mandalas('mandalas')
 setImageProfile()
 Skills.Preset.forEach(Skills.setSkill)
@@ -15,9 +15,13 @@ const presentationList = [
     'return name',
     'Bruno Salvo',
 ]
-const presentation = new TypeEffect('presentation',presentationList)
-
-const sandboxForm = new Form({name:'sandbox',action:'google.com.br', el:'#sandboxForm'});
+const presentation = new TypeEffect('presentation', presentationList)
+const inputs = [
+    { name: 'email', label: 'Email', type: 'email', placeholder: 'Qual seu email?', validation: { type: 'required' } },
+    { name: 'senha', label: 'Senha', type: 'password', placeholder: 'Qual sua senha?', validation: { type: 'required' } },
+]
+const sandboxForm = new Form({ name: 'sandbox', action: 'google.com.br', parent: '#sandboxForm', inputs: inputs });
+window.teste = sandboxForm
 
 
 // const btn_add_petala = document.getElementById("mandala_add_petela")
